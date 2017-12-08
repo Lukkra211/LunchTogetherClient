016dd50eb9356ec67dce0fa912c93e595f5b1166
Vojtaech <kudlacek.vojtech@sspbrno.cz>
2017-12-08 14:41:01 +0100

    Created registerActivity and added editTexts to it

---
+++ app/src/main/java/lunch_together/purkynova/com/lunchtogetherclient/registerActivity.java
@@ -0,0 +1,13 @@
+package lunch_together.purkynova.com.lunchtogetherclient;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class registerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
