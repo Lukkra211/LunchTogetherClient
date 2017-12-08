98cee1f8f8d60300387c6b2b11e51c6394ab8fac
Vojtěch Kalivoda <vojta.kalivoda@gmail.com>
2017-12-08 19:04:14 +0100

    Add Login and Register methods

--- app/src/main/java/lunch_together/purkynova/com/lunchtogetherclient/model/Model.java
+++ app/src/main/java/lunch_together/purkynova/com/lunchtogetherclient/model/Model.java
@@ -1,6 +1,7 @@
 package lunch_together.purkynova.com.lunchtogetherclient.model;
 
 import android.content.Context;
+import android.os.AsyncTask;
 import android.util.Log;
 
 import com.android.volley.Request;
@@ -9,105 +10,66 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.RequestFuture;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 import lunch_together.purkynova.com.lunchtogetherclient.representation.Event;
+import lunch_together.purkynova.com.lunchtogetherclient.representation.User;
 
 /**
  * Created by vojtech on 12/8/17.
  */
 
-public abstract class Client
+public class Model
 {
-    private final String serverURL = "10.10.5.209";
-    private RequestQueue requestQueue;
-
-
-    public void init(Context context)
+    public String register(String name, String mail, String password)
     {
-        requestQueue = Volley.newRequestQueue(context);
-    }
-
-    //return true/false
-    public boolean register(String name, String email, String password)
-    {
-        RequestFuture<JSONObject> future = RequestFuture.newFuture();
-        JsonObjectRequest request = new JsonObjectRequest(serverURL+"/request", new JSONObject(), future, future);
-        requestQueue.add(request);
-
         try
         {
-            JSONObject response = future.get();
-            //Parse JSON
+            return new Register().execute(name,password,mail).get();
         }
         catch (Exception ex)
         {
             ex.printStackTrace();
         }
-
-        return false;
+        return "";
     }
 
-    //return id
-    public int login()
+    public int login(String name, String password)
     {
-        RequestFuture<JSONObject> future = RequestFuture.newFuture();
-        JsonObjectRequest request = new JsonObjectRequest(serverURL+"/login", new JSONObject(), future, future);
-        requestQueue.add(request);
-
         try
         {
-            JSONObject response = future.get();
-            //Parse JSON
+            return new Login().execute(name,password).get();
         }
         catch (Exception ex)
         {
             ex.printStackTrace();
         }
-
         return -1;
     }
 
-    public Event getUsersEvent()
+    public Event getEvent()
     {
-        RequestFuture<JSONObject> future = RequestFuture.newFuture();
-        JsonObjectRequest request = new JsonObjectRequest(serverURL+"/getUsersEvent", new JSONObject(), future, future);
-        requestQueue.add(request);
-
-        try
-        {
-            JSONObject response = future.get();
-            //Parse JSON
-        }
-        catch (Exception ex)
-        {
-            ex.printStackTrace();
-        }
-
         return null;
     }
 
-    public Event getAllEvents()
+    public ArrayList<Event> getEventList()
     {
-        RequestFuture<JSONObject> future = RequestFuture.newFuture();
-        JsonObjectRequest request = new JsonObjectRequest(serverURL+"/getAllEvents", new JSONObject(), future, future);
-        requestQueue.add(request);
-
-        try
-        {
-            JSONObject response = future.get();
-            //Parse JSON
-        }
-        catch (Exception ex)
-        {
-            ex.printStackTrace();
-        }
-
-        return null;
+        return new ArrayList<Event>();
     }
-
 }
