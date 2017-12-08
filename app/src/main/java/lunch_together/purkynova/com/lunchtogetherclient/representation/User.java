66adc8aa1c764aa251342a66383182e62285586f
Lukáš Krajíček <krajicek.lukkra@gmail.com>
2017-12-08 18:45:56 +0100

    Add User class

--- app/src/main/java/lunch_together/purkynova/com/lunchtogetherclient/representation/User.java
+++ app/src/main/java/lunch_together/purkynova/com/lunchtogetherclient/representation/User.java
@@ -5,7 +5,13 @@ package lunch_together.purkynova.com.lunchtogetherclient.representation;
  */
 
 public final class User {
-    public User(int id, String fullname, String mail) {
+    public final int id;
+    public final String fullname;
+    public final String mail;
 
+    public User(int id, String fullname, String mail) {
+        this.id = id;
+        this.fullname = fullname;
+        this.mail = mail;
     }
 }
