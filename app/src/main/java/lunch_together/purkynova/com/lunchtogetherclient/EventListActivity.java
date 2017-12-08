fb254d1ee397d0a5a3e745833dc15f3d2133594e
Lukáš Krajíček <krajicek.lukkra@gmail.com>
2017-12-08 14:49:09 +0100

    Rename ListActivity to EventListActivity

--- app/src/main/java/lunch_together/purkynova/com/lunchtogetherclient/EventListActivity.java
+++ app/src/main/java/lunch_together/purkynova/com/lunchtogetherclient/EventListActivity.java
@@ -1,13 +1,23 @@
 package lunch_together.purkynova.com.lunchtogetherclient;
 
+import android.app.ListActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
-public class ListActivity extends AppCompatActivity {
+public class EventListActivity extends ListActivity {
+
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+        initParams();
+    }
+
+    private void initParams() {
+        this.listView = getListView();
     }
 }
