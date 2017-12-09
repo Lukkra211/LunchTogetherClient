d83fff496462c1fffdb9a5b9105d84923d5485ef
Vojtaech <kudlacek.vojtech@sspbrno.cz>
2017-12-09 01:08:20 +0100

    .

--- app/src/main/java/lunch_together/purkynova/com/lunchtogetherclient/EventActivity.java
+++ app/src/main/java/lunch_together/purkynova/com/lunchtogetherclient/EventActivity.java
@@ -14,36 +14,5 @@ public class EventActivity extends AppCompatActivity {
         setContentView(R.layout.activity_event);
 
     }
-    public void launchMaps(int x,int y)
-    {
-        Uri gmmIntentUri = Uri.parse("geo:"+x+","+y+"?q="+x+","+y+"");
-        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
-        mapIntent.setPackage("com.google.android.apps.maps");
-        if (mapIntent.resolveActivity(getPackageManager()) != null) {
-            startActivity(mapIntent);
-        }
-    }
-    public double getDistanceOfTwoPoints(double x1,double y1,double x2,double y2)
-    {
-        double distace;
-        double dx;
-        double dy;
-        final double convert = 110.574f;
-        if( x1 > x2)
-        {
-            dx = x1-x2;
-        }else
-        {
-            dx = x2-x1;
-        }
 
-        if( y1 > y2)
-        {
-            dy = y1-y2;
-        }else
-        {
-            dy = y2-y1;
-        }
-        return Math.sqrt(Math.pow(dy * convert,2) + Math.pow(dx * convert,2));
-    }
 }
