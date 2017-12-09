0cc8f13b80cd630ed0c461e441b2ba4f8857bae6
Vojtaech <kudlacek.vojtech@sspbrno.cz>
2017-12-09 03:11:45 +0100

    Added dialog to show info about user

--- app/src/main/java/lunch_together/purkynova/com/lunchtogetherclient/EventActivity.java
+++ app/src/main/java/lunch_together/purkynova/com/lunchtogetherclient/EventActivity.java
@@ -1,15 +1,22 @@
 package lunch_together.purkynova.com.lunchtogetherclient;
 
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.graphics.Typeface;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import lunch_together.purkynova.com.lunchtogetherclient.model.Model;
+import lunch_together.purkynova.com.lunchtogetherclient.representation.User;
+
 public class EventActivity extends AppCompatActivity
 {
 
+    AlertDialog.Builder builder;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -17,8 +24,17 @@ public class EventActivity extends AppCompatActivity
         setContentView(R.layout.activity_event);
 
         ListView attendersLV = findViewById(R.id.attendersLV);
-        String[] attenders = new String[]{"Jan Novak","Petr Horak", "Jana fsadfaskj", "fjsdhfkj sdkjf"};
+        final String[] attenders = new String[]{"Jan Novak","Petr Horak", "Jana fsadfaskj", "fjsdhfkj sdkjf"};
         attendersLV.setAdapter(new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,attenders));
+        builder = new AlertDialog.Builder(this);
+        attendersLV.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                builder.setMessage("Jméno: " + attenders[(int)l] +"\nEmail: " + attenders[(int)l]);
+              //  builder.setMessage("Email: " + attenders[(int)l]);
+                builder.create().show();
+            }
+        });
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
